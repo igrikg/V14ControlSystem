6aeeebe1a8b822b684056fd106f76e8f21baaf61
v14user <igrikg@gmail.com>
2023-02-19 13:05:11 +0100

    new class DaQ

---
+++ DAQ/NiPCI6602.py
@@ -0,0 +1,10 @@
+from PyDAQmx.DAQmxFunctions import *
+from PyDAQmx.DAQmxConstants import *
+
+class NiPci6602():
+    """ Class to create a
+
+    Usage:
+    """
+    def __init__(self, counter="Dev1/ctr0", reset=False):
+        pass
