083deac0736c3734acb9575b41400e165451d0de
v14user <igrikg@gmail.com>
2023-02-20 23:00:49 +0100

    some changes

--- DAQ/NiPCI6602.py
+++ DAQ/NiPCI6602.py
@@ -8,3 +8,16 @@ class NiPci6602():
     """
     def __init__(self, counter="Dev1/ctr0", reset=False):
         pass
+
+from PyDAQmx import Task
+import PyDAQmx
+import numpy as np
+
+
+data = np.array([0,1,1,0,1,0,1,0], dtype=np.uint8)
+
+task = Task()
+task.CreateDOChan("/Dev1/port0/line0:7","",PyDAQmx.DAQmx_Val_ChanForAllLines)
+task.StartTask()
+task.WriteDigitalLines(1,1,10.0,PyDAQmx.DAQmx_Val_GroupByChannel,data,None,None)
+task.StopTask()
